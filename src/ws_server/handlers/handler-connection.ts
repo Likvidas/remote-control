b7890e2dd6b57d5c44ddfb6e60bb42835e3cb1e4
Likvidas <bobento@yandex.ru>
2023-01-23 13:14:16 +0300

    feat: added handling mouse events

---
+++ src/ws_server/handlers/handler-connection.ts
@@ -0,0 +1,35 @@
+import { WebSocket, createWebSocketStream } from 'ws';
+import { mouse, up, down, left, right } from '@nut-tree/nut-js';
+import { Commands } from './handler-connection.types';
+
+export const handleWsConnection = async (ws: WebSocket) => {
+  const wsStream = createWebSocketStream(ws, {
+    encoding: 'utf8',
+    decodeStrings: false,
+  });
+
+  wsStream.on('data', async (chunk: Buffer) => {
+    const [command, mainValue] = chunk.toString().split(' ');
+
+    switch (command) {
+      case Commands.MouseUp:
+        await mouse.move(up(Number(mainValue)));
+        wsStream.write(`${command}_${mainValue}`);
+        break;
+      case Commands.MouseDown:
+        await mouse.move(down(Number(mainValue)));
+        wsStream.write(`${command}_${mainValue}`);
+        break;
+      case Commands.MouseLeft:
+        await mouse.move(left(Number(mainValue)));
+        wsStream.write(`${command}_${mainValue}`);
+        break;
+      case Commands.MouseRight:
+        await mouse.move(right(Number(mainValue)));
+        wsStream.write(`${command}_${mainValue}`);
+        break;
+      default:
+        break;
+    }
+  });
+};
--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -1,13 +1,23 @@
 import { WebSocketServer } from 'ws';
+import * as process from 'process';
+import { handleWsConnection } from './handlers/handler-connection';
 
 export const wsServer = (port: number) => {
   const ws = new WebSocketServer({ port });
 
-  ws.on('listening', () => console.log(`WebSocket Server started at http://127.0.0.1:${port}`));
+  const handleAppExit = async () => {
+    await ws.close();
+    process.exit(0);
+  };
 
-  ws.on('close', () => console.log('WebSocket Server stoped'));
+  ws.on('listening', () => console.log(`WebSocket Server started on the ${port}`));
 
-  ws.on('connection', () => console.log('someone connected!'));
+  ws.on('close', () => console.log('WebSocket Server stoped. Goodbye'));
+
+  ws.on('connection', handleWsConnection);
+
+  process.on('SIGINT', handleAppExit);
+  process.on('exit', handleAppExit);
 
   return ws;
 };
